bb1f24d89726516a4c522d582745c1af857f2001
highhead <highhead@gmx.at>
2016-05-20 21:23:39 +0200

    [MAIN] Fixed ALiVE menus not working with ArmA 3 1.60

--- addons/main/CfgPatches.hpp
+++ addons/main/CfgPatches.hpp
@@ -4,7 +4,7 @@ class CfgPatches {
 		units[] = { };
 		weapons[] = { };
 		requiredVersion = REQUIRED_VERSION;
-		requiredAddons[] = { "A3_Modules_F", "cba_xeh_a3" };
+		requiredAddons[] = { "A3_Modules_F", "cba_xeh_a3", "CBA_events" };
 		versionDesc = "ALiVE";
 		//versionAct = "['MAIN',_this] execVM '\x\alive\addons\main\about.sqf';";
 		VERSION_CONFIG;
