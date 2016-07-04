667b7d752ac22ca11a9fbe5e007cb074f76bfaea
Tupolov <tupolov73@gmail.com>
2016-07-04 15:43:03 +0100

    [splashscreen] Convert tabs to spaces

--- addons/splashscreen/script_component.hpp
+++ addons/splashscreen/script_component.hpp
@@ -4,11 +4,11 @@
 //#define DEBUG_ENABLED_splashscreen
 
 #ifdef DEBUG_ENABLED_splashscreen
-	#define DEBUG_MODE_FULL
+    #define DEBUG_MODE_FULL
 #endif
 
 #ifdef DEBUG_SETTINGS_splashscreen
-	#define DEBUG_SETTINGS DEBUG_SETTINGS_splashscreen
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_splashscreen
 #endif
 
 #include <\x\cba\addons\main\script_macros.hpp>
