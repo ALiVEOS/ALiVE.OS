3ec6fd0b5b3b4c5aa50ef06eed30ce1f7641e376
Tupolov <tupolov73@gmail.com>
2016-07-04 15:41:30 +0100

    [mil_placement_custom] Convert tabs to spaces

--- addons/mil_placement_custom/script_component.hpp
+++ addons/mil_placement_custom/script_component.hpp
@@ -2,11 +2,11 @@
 #include <\x\alive\addons\main\script_mod.hpp>
 
 #ifdef DEBUG_ENABLED_MIL_PLACEMENT_CUSTOM
-	#define DEBUG_MODE_FULL
+    #define DEBUG_MODE_FULL
 #endif
 
 #ifdef DEBUG_SETTINGS_mil_placement_custom
-	#define DEBUG_SETTINGS DEBUG_ENABLED_MIL_PLACEMENT_CUSTOM
+    #define DEBUG_SETTINGS DEBUG_ENABLED_MIL_PLACEMENT_CUSTOM
 #endif
 
 #include <\x\cba\addons\main\script_macros.hpp>
