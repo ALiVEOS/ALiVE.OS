dd919acdf650b65ff410d8b567c8bf42d74ff8f5
Tupolov <tupolov73@gmail.com>
2016-07-13 15:06:57 -0700

    [main] Added faction mapping for BIS Gendarmarie faction

--- addons/main/static/CustomFactions.hpp
+++ addons/main/static/CustomFactions.hpp
@@ -33,6 +33,27 @@ BLU_G_F_typeMappings = [] call ALIVE_fnc_hashCreate;
 [ALIVE_factionCustomMappings, "BLU_G_F", BLU_G_F_mappings] call ALIVE_fnc_hashSet;
 // ---------------------------------------------------------------------------------------------------------------------
 
+// ---------------------------------------------------------------------------------------------------------------------
+BLU_GEN_F_mappings = [] call ALIVE_fnc_hashCreate;
+[BLU_GEN_F_mappings, "Side", "WEST"] call ALIVE_fnc_hashSet;
+[BLU_GEN_F_mappings, "GroupSideName", "WEST"] call ALIVE_fnc_hashSet;
+[BLU_GEN_F_mappings, "FactionName", "Gendarmarie"] call ALIVE_fnc_hashSet;
+[BLU_GEN_F_mappings, "GroupFactionName", "BLU_GEN_F"] call ALIVE_fnc_hashSet;
+
+BLU_GEN_F_typeMappings = [] call ALIVE_fnc_hashCreate;
+[BLU_GEN_F_typeMappings, "Air", "Air"] call ALIVE_fnc_hashSet;
+[BLU_GEN_F_typeMappings, "Armored", "Armored"] call ALIVE_fnc_hashSet;
+[BLU_GEN_F_typeMappings, "Infantry", "Infantry"] call ALIVE_fnc_hashSet;
+[BLU_GEN_F_typeMappings, "Mechanized", "Mechanized"] call ALIVE_fnc_hashSet;
+[BLU_GEN_F_typeMappings, "Motorized", "Motorized"] call ALIVE_fnc_hashSet;
+[BLU_GEN_F_typeMappings, "Motorized_MTP", "Motorized_MTP"] call ALIVE_fnc_hashSet;
+[BLU_GEN_F_typeMappings, "SpecOps", "SpecOps"] call ALIVE_fnc_hashSet;
+[BLU_GEN_F_typeMappings, "Support", "Support"] call ALIVE_fnc_hashSet;
+
+[BLU_GEN_F_mappings, "GroupFactionTypes", BLU_GEN_F_typeMappings] call ALIVE_fnc_hashSet;
+[ALIVE_factionCustomMappings, "Gendarmarie", BLU_GEN_F_mappings] call ALIVE_fnc_hashSet;
+// ---------------------------------------------------------------------------------------------------------------------
+
 
 // African
 // ---------------------------------------------------------------------------------------------------------------------
