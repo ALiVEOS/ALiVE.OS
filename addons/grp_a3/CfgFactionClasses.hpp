0a3cb603ac6b6a8bd85351a92b218e6df15e0f44
SpyderBlack723 <spyderblack8@gmail.com>
2016-07-14 23:01:36 -0500

    [grp_a3] Added new faction class definitions

---
+++ addons/grp_a3/CfgFactionClasses.hpp
@@ -0,0 +1,29 @@
+class CfgFactionClasses {
+    class IND_G_F {
+        displayName = "FIA";
+        priority = 3;
+        side = 2;
+    };
+
+    class CIV_F;
+    class CIV_F_EUROPE : CIV_F {
+        displayName = "Civilians (European)";
+        priority = 2;
+        side = 3;
+    };
+    class CIV_F_AFRICA : CIV_F {
+        displayName = "Civilians (African)";
+        priority = 2;
+        side = 3;
+    };
+    class CIV_F_ASIA : CIV_F {
+        displayName = "Civilians (Asian)";
+        priority = 2;
+        side = 3;
+    };
+    class CIV_F_TANOA : CIV_F {
+        displayName = "Civilians (Tanoan)";
+        priority = 4;
+        side = 3;
+    };
+};
