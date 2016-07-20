7a0c632eedcbb8345775ad3b2fdcd8e2765d26f5
Tupolov <tupolov73@gmail.com>
2016-07-20 14:12:56 +0100

    [main] Added new APEX buildings to garrison position static data

--- addons/main/static/GarrisonPositions.hpp
+++ addons/main/static/GarrisonPositions.hpp
@@ -19,3 +19,15 @@ ALIVE_garrisonPositions = [] call ALIVE_fnc_hashCreate;
 [ALIVE_garrisonPositions, "Land_i_Barracks_V2_F", [36,37,35,34,32,33,40,44]] call ALIVE_fnc_hashSet;
 [ALIVE_garrisonPositions, "Land_BagBunker_Small_F", [2,3]] call ALIVE_fnc_hashSet;
 [ALIVE_garrisonPositions, "Land_BagFence_Round_F", [1]] call ALIVE_fnc_hashSet;
+
+// APEX
+[ALIVE_garrisonPositions,"Land_Barracks_01_dilapidated_F", [36,33,35,34,31,30,29,10,11]] call ALIVE_fnc_hashSet;
+[ALIVE_garrisonPositions,"Land_BagBunker_01_large_green_F", [2,3,4,5,6,7,8,9,10,11,12,13]] call ALIVE_fnc_hashSet;
+[ALIVE_garrisonPositions,"Land_BagBunker_01_small_green_F", [2,3]] call ALIVE_fnc_hashSet;
+[ALIVE_garrisonPositions,"Land_Cargo_House_V4_F", [1]] call ALIVE_fnc_hashSet;
+[ALIVE_garrisonPositions,"Land_Cargo_HQ_V4_F", [2,3,4,5,6,7,8,9,10,11,12,13]] call ALIVE_fnc_hashSet;
+[ALIVE_garrisonPositions,"Land_Cargo_Patrol_V4_F", [1,2]] call ALIVE_fnc_hashSet;
+[ALIVE_garrisonPositions,"Land_Cargo_Tower_V4_F", [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]] call ALIVE_fnc_hashSet;
+[ALIVE_garrisonPositions,"Land_PillboxBunker_01_big_F", [2,3,4,5,6]] call ALIVE_fnc_hashSet;
+[ALIVE_garrisonPositions,"Land_PillboxBunker_01_hex_F", [2,3,4,5]] call ALIVE_fnc_hashSet;
+[ALIVE_garrisonPositions,"Land_PillboxBunker_01_rectangle_F", [2,3,4]] call ALIVE_fnc_hashSet;
