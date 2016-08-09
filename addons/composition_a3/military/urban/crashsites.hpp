f10ba9fa97ede5e418f466179a8ab39d4c9162de
Tupolov <tupolov73@gmail.com>
2016-08-09 11:16:14 +0100

    [composition_a3] Added ALiVE crashsites and IED factory

--- addons/composition_a3/military/urban/crashsites.hpp
+++ addons/composition_a3/military/urban/crashsites.hpp
@@ -5,8 +5,62 @@
     class CrashsitesMedium
     {
         name = $STR_ZEC_CrashsitesMedium;
-    };
+        class mediumc192Crash1
+        {
+              name = "$STR_ALIVE_COMPOSITION_mediumc192Crash1";
+              class Object1 {side=8;vehicle="Land_BagFence_Round_F";rank="";position[]={-0.141113,-1.72705,-0.00561666};dir=209;};
+              class Object2 {side=8;vehicle="Land_BagFence_Long_F";rank="";position[]={2.47437,-0.635254,-0.00531578};dir=79;};
+              class Object3 {side=8;vehicle="Land_BagFence_Long_F";rank="";position[]={2.41846,2.20166,-0.00387716};dir=96;};
+              class Object4 {side=8;vehicle="Land_BagFence_Round_F";rank="";position[]={-0.232422,-4.00342,-0.00561666};dir=331;};
+              class Object5 {side=8;vehicle="O_HMG_01_high_F";rank="";position[]={-1.99829,-3.47803,-0.088551};dir=87.0088;};
+              class Object6 {side=8;vehicle="Land_BagFence_Long_F";rank="";position[]={2.65967,-3.64893,-0.00531578};dir=270;};
+              class Object7 {side=8;vehicle="Land_BagFence_Long_F";rank="";position[]={2.68604,-6.56006,-0.00531578};dir=270;};
+              class Object8 {side=8;vehicle="Land_Razorwire_F";rank="";position[]={4.4978,-5.99609,-0.00431824};dir=306;};
+              class Object9 {side=8;vehicle="Land_Razorwire_F";rank="";position[]={6.29761,4.83447,-0.00431824};dir=71;};
+              class Object10 {side=8;vehicle="Land_BagFence_Long_F";rank="";position[]={-2.55615,-7.57715,0.818771};dir=21;};
+              class Object11 {side=8;vehicle="Land_BagFence_Long_F";rank="";position[]={-0.315186,-8.00049,-0.0409451};dir=3;};
+              class Object12 {side=8;vehicle="Land_BagFence_Long_F";rank="";position[]={-3.08276,-7.39746,-0.027421};dir=23;};
+              class Object13 {side=8;vehicle="Land_BagFence_Long_F";rank="";position[]={0.244873,-8.02051,0.753727};dir=2;};
+              class Object14 {side=8;vehicle="Land_BagFence_Short_F";rank="";position[]={2.052,-8.2251,-0.00531578};dir=0;};
+              class Object15 {side=8;vehicle="Land_Wreck_Plane_Transport_01_F";rank="";position[]={-4.50073,7.56982,-0.00431585};dir=0;};
+              class Object16 {side=8;vehicle="Land_Razorwire_F";rank="";position[]={1.35571,11.7852,-0.00431824};dir=48;};
+        };
+};
     class CrashsitesSmall
     {
         name = $STR_ZEC_CrashsitesSmall;
+       class smallOspreyCrashsite1
+        {
+              name = "$STR_ALIVE_COMPOSITION_smallOspreyCrashsite1";
+              class Object1 {side=8;vehicle="Land_Ammobox_rounds_F";rank="";position[]={0.153076,-0.882813,-0.00160456};dir=266.005;};
+              class Object2 {side=8;vehicle="Land_Ammobox_rounds_F";rank="";position[]={-0.00366211,-0.90918,-0.00160217};dir=266.003;};
+              class Object3 {side=8;vehicle="Land_Ammobox_rounds_F";rank="";position[]={-0.158203,-0.930176,-0.00160217};dir=266.003;};
+              class Object4 {side=8;vehicle="Land_Ammobox_rounds_F";rank="";position[]={-0.314209,-0.927246,-0.00160217};dir=266.003;};
+              class Object5 {side=8;vehicle="Campfire_burning_F";rank="";position[]={-0.350342,1.3501,0.0256829};dir=0;};
+              class Object6 {side=8;vehicle="Land_BakedBeans_F";rank="";position[]={-1.23315,0.721191,-0.00139713};dir=344.99;};
+              class Object7 {side=8;vehicle="Land_Sleeping_bag_brown_F";rank="";position[]={-2.11646,0.150391,0.0225549};dir=180;};
+              class Object8 {side=8;vehicle="Land_Camping_Light_off_F";rank="";position[]={1.16187,-2.69678,-0.00173712};dir=359.023;};
+              class Object9 {side=8;vehicle="Land_Sleeping_bag_brown_F";rank="";position[]={-0.960693,-2.49707,-0.00431585};dir=90;};
+              class Object10 {side=8;vehicle="Land_CampingChair_V1_F";rank="";position[]={-0.287598,2.93945,0.00168514};dir=7.00778;};
+              class Object11 {side=8;vehicle="Land_Sleeping_bag_brown_folded_F";rank="";position[]={-3.04126,0.149414,-0.00143862};dir=87;};
+              class Object12 {side=8;vehicle="Land_HumanSkeleton_F";rank="";position[]={2.62329,-1.59863,0.344727};dir=210.072;};
+              class Object13 {side=8;vehicle="Land_cargo_addon01_V1_F";rank="";position[]={-1.42725,2.73926,-0.0775042};dir=161;};
+              class Object14 {side=8;vehicle="Land_SurvivalRadio_F";rank="";position[]={-2.72266,1.8877,-0.00143814};dir=130;};
+              class Object15 {side=8;vehicle="Land_Sleeping_bag_brown_F";rank="";position[]={-2.62061,2.67041,-0.00431585};dir=268;};
+              class Object16 {side=8;vehicle="Land_cargo_addon02_V1_F";rank="";position[]={-1.98315,-3.16016,-0.00431585};dir=0;};
+              class Object17 {side=8;vehicle="Land_UWreck_MV22_F";rank="";position[]={2.40991,1.25244,-0.00431585};dir=0;};
+              class Object18 {side=8;vehicle="Land_CanisterFuel_F";rank="";position[]={-4.90234,-1.83838,-0.0013442};dir=356.058;};
+              class Object19 {side=8;vehicle="Land_Shovel_F";rank="";position[]={-5.00928,-2.62646,0.00236225};dir=55.0511;};
+              class Object20 {side=8;vehicle="Oil_Spill_F";rank="";position[]={-6.23193,-2.38672,-0.00431585};dir=0;};
+              class Object21 {side=8;vehicle="Land_Camping_Light_off_F";rank="";position[]={2.05493,7.83887,-0.00173712};dir=359.023;};
+        };
+        class smallAH99Crashsite1
+        {
+              name = "$STR_ALIVE_COMPOSITION_smallAH99Crashsite1";
+              class Object1 {side=8;vehicle="Land_Sleeping_bag_F";rank="";position[]={1.62402,0.90918,-0.00431585};dir=248;};
+              class Object2 {side=8;vehicle="Campfire_burning_F";rank="";position[]={2.97095,-1.00098,-0.00431585};dir=0;};
+              class Object3 {side=8;vehicle="Land_Wreck_Heli_Attack_01_F";rank="";position[]={-2.78296,0.727539,-0.00431585};dir=0;};
+              class Object4 {side=8;vehicle="Land_TentA_F";rank="";position[]={0.789063,4.29004,0.000490665};dir=0;};
+              class Object5 {side=8;vehicle="Land_Sleeping_bag_F";rank="";position[]={2.97437,-3.34473,-0.00431585};dir=268;};
+        };
     };
