ed18e4a326f56e0f68cffaaf3c2c0f961e5b8e71
Jman <jman@kellys-heroes.eu>
2016-10-25 11:19:19 +0100

    [ALL] Updated version to 1.2.1.1610251 clusterbuild 1.64.138732 - Public Stable (hotfix)

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -3,18 +3,18 @@
 
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
 #define MAJOR 1
-#define MINOR 0
-#define PATCHLVL 5
+#define MINOR 2
+#define PATCHLVL 1
 // BUILD should be YYMMDDR eg 1311211
-#define BUILD 1605051
+#define BUILD 1610251
 
-#define CLUSTERBUILD "Arma 3","Arma3",158,135742,"Stable"
+#define CLUSTERBUILD "Arma 3","Arma3",164,138732,"Stable"
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
 
 // MINIMAL required version for the Mod. Components can specify others..
-#define REQUIRED_VERSION 1.58
+#define REQUIRED_VERSION 1.64
 
 /*
  // Defined DEBUG_MODE_NORMAL in a few CBA_fncs to prevent looped logging :)
@@ -61,5 +61,5 @@
 #ifdef RECOMPILE
     #undef RECOMPILE
 #endif
-#define RECOMPILE recompile = 1
+#define RECOMPILE recompile = 0
 #define MODULE_AUTHOR QUOTE(ALiVE Mod Team)
