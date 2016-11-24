1eeda27afe1d0502ba92588ef6f688d8850d563f
Jman <jman@kellys-heroes.eu>
2016-11-24 12:12:50 +0000

    * [ALL] Updated version to 1.2.2.1611241

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -6,7 +6,7 @@
 #define MINOR 2
 #define PATCHLVL 2
 // BUILD should be YYMMDDR eg 1311211
-#define BUILD 1611141
+#define BUILD 1611241
 
 #define CLUSTERBUILD "Arma 3","Arma3",164,138732,"Stable"
 
