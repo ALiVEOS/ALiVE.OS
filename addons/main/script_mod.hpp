9d624fefce9c2516a464ba2efe5fe614c4561378
Jman <jman@kellys-heroes.eu>
2017-04-26 22:08:46 +0100

    [ALL] Updated version to 1.3.2.1704261

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -4,9 +4,9 @@
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
 #define MAJOR 1
 #define MINOR 3
-#define PATCHLVL 1
+#define PATCHLVL 2
 // BUILD should be YYMMDDR eg 1311211
-#define BUILD 1704141
+#define BUILD 1704261
 
 #define CLUSTERBUILD "Arma 3","Arma3",168,141205,"Stable"
 
