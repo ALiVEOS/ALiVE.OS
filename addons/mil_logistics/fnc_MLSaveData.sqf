ALiVEOS/ALiVE.OS

commit 2947cf3cfb79a27bbe75388356bcd934c5db8ac5

[VARIOUS] Added option to SYS DATA to persist mission state locally to profileNamespace, which enables persistence for SP, hosted and dedicated server. Integrates with current ALiVE interface and uses already given module-export-data. Does not use @ALiVEServer and therefore does not support webservices (for statistics, AAR, perf monitoring etc. use the proper CouchDB way). Select "ProfileNamespace" on the ALiVE Database module and make sure you have persistence enabled on the other modules, which you want to persist! WARNING: Experimental! Back up your current user and/or server profile folder or define a new one in your startup params! Additional functions: call ALiVE_fnc_ProfileNameSpaceClear in debug >>> will wipe the current mission data (or rename the mission)! call ALiVE_fnc_ProfileNameSpaceWipe will delete all ALiVE related data from profileNamespace (ergo: delete all missions).

addons/mil_logistics/fnc_MLSaveData.sqf
@@ -27,7 +27,7 @@ ARJay
 
 private ["_result","_data","_async","_missionName","_message","_messages","_saveResult"];
 
-if !(isDedicated && {!(isNil "ALIVE_sys_data")} && {!(ALIVE_sys_data_DISABLED)}) exitwith {false};
+if !(isServer && {!(isNil "ALIVE_sys_data")} && {!(ALIVE_sys_data_DISABLED)}) exitwith {false};
 
 if(ALiVE_SYS_DATA_DEBUG_ON) then {
     [true, "ALiVE MIL LOGISTICS - Saving data", "mlper"] call ALIVE_fnc_timer;
@@ -65,7 +65,7 @@ if (isNil QGVAR(DATAHANDLER)) then {
     [GVAR(DATAHANDLER),"storeType",true] call ALIVE_fnc_Data;
 };
 
-_saveResult = [GVAR(DATAHANDLER), "write", ["mil_logistics", _data, _async, _missionName]] call ALIVE_fnc_Data;
+_saveResult = [GVAR(DATAHANDLER), "bulkSave", ["mil_logistics", _data, _missionName,_async]] call ALIVE_fnc_Data;
 _result set [0,_saveResult];
 
 _message = format["ALiVE Military Logistics - Save Result: %1",_saveResult];

commit f878a0cd0e90509899a748c89a9138e05fd063f8

[MIL LOGISTICS] Fix regression introduced with https://github.com/ALiVEOS/ALiVE.OS/commit/2947cf3cfb79a27bbe75388356bcd934c5db8ac5 (fix #341)

addons/mil_logistics/fnc_MLSaveData.sqf
@@ -65,7 +65,7 @@ if (isNil QGVAR(DATAHANDLER)) then {
     [GVAR(DATAHANDLER),"storeType",true] call ALIVE_fnc_Data;
 };
 
-_saveResult = [GVAR(DATAHANDLER), "bulkSave", ["mil_logistics", _data, _missionName,_async]] call ALIVE_fnc_Data;
+_saveResult = [GVAR(DATAHANDLER), "write", ["mil_logistics", _data, _async, _missionName]] call ALIVE_fnc_Data;
 _result set [0,_saveResult];
 
 _message = format["ALiVE Military Logistics - Save Result: %1",_saveResult];