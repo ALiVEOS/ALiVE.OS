d5805c3349032d80fa924cef6ae23ca6a8b138b3
Tupolov <tupolov73@gmail.com>
2017-07-07 11:33:00 +0100

    [main] Update Master clusterbuild and ALiVE version

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -3,18 +3,18 @@
 
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
 #define MAJOR 1
-#define MINOR 0
-#define PATCHLVL 5
+#define MINOR 3
+#define PATCHLVL 7
 // BUILD should be YYMMDDR eg 1311211
-#define BUILD 1605051
+#define BUILD 1706291
 
-#define CLUSTERBUILD "Arma 3","Arma3",158,135742,"Stable"
+#define CLUSTERBUILD "Arma 3","Arma3",172,142223,"Stable"
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
 
 // MINIMAL required version for the Mod. Components can specify others..
-#define REQUIRED_VERSION 1.58
+#define REQUIRED_VERSION 1.70
 
 /*
  // Defined DEBUG_MODE_NORMAL in a few CBA_fncs to prevent looped logging :)
