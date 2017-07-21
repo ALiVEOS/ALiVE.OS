dfe116c4d44f945f24537fcfd44537afad2cfc82
Marcel de Vries <marceldev89@gmail.com>
2017-07-21 21:19:40 +0200

    [composition_cup] Tabs => spaces

--- optional/composition_cup/CfgGroups.hpp
+++ optional/composition_cup/CfgGroups.hpp
@@ -1,71 +1,71 @@
 class CfgGroups
 {
-	class Empty
-	{
-		side = 8;
-		name = "Compositions";
-		class Civilian
-		{
+    class Empty
+    {
+        side = 8;
+        name = "Compositions";
+        class Civilian
+        {
             name = "Civilian";
             #include "civilian\urban.hpp"
         };
-	    class Civilian_Desert
-	    {
-	    	name = "Civilian (Desert)";
-	    	#include "civilian\desert.hpp"
-	    };
-	    class Civilian_Woodland
-	    {
-	    	name = "Civilian (Woodland)";
-	    	#include "civilian\woodland.hpp"
-	    };
-	    class Civilian_Pacific
-	    {
-	    	name = "Civilian (Pacific)";
-	    	#include "civilian\pacific.hpp"
-	    };
+        class Civilian_Desert
+        {
+            name = "Civilian (Desert)";
+            #include "civilian\desert.hpp"
+        };
+        class Civilian_Woodland
+        {
+            name = "Civilian (Woodland)";
+            #include "civilian\woodland.hpp"
+        };
+        class Civilian_Pacific
+        {
+            name = "Civilian (Pacific)";
+            #include "civilian\pacific.hpp"
+        };
 
-		class Guerrilla
-		{
+        class Guerrilla
+        {
             name = "Guerrilla";
             #include "guerrilla\urban.hpp"
         };
-	    class Guerrilla_Desert
-	    {
-	    	name = "Guerrilla (Desert)";
-	    	#include "guerrilla\desert.hpp"
-	    };
-	    class Guerrilla_Woodland
-	    {
-	    	name = "Guerrilla (Woodland)";
-	    	#include "guerrilla\woodland.hpp"
-	    };
-	    class Guerrilla_Pacific
-	    {
-	    	name = "Guerrilla (Pacific)";
-	    	#include "guerrilla\pacific.hpp"
-	    };
+        class Guerrilla_Desert
+        {
+            name = "Guerrilla (Desert)";
+            #include "guerrilla\desert.hpp"
+        };
+        class Guerrilla_Woodland
+        {
+            name = "Guerrilla (Woodland)";
+            #include "guerrilla\woodland.hpp"
+        };
+        class Guerrilla_Pacific
+        {
+            name = "Guerrilla (Pacific)";
+            #include "guerrilla\pacific.hpp"
+        };
 
-		class Military
-		{
+        class Military
+        {
             name = "Military";
             #include "military\urban.hpp"
         };
-	    class Military_Desert
-	    {
-	    	name = "Military (Desert)";
-	    	#include "military\desert.hpp"
-	    };
-	    class Military_Woodland
-	    {
-	    	name = "Military (Woodland)";
-	    	#include "military\woodland.hpp"
-	    };
-	    class Military_Pacific
-	    {
-	    	name = "Military (Pacific)";
-	    	#include "military\pacific.hpp"
-	    };
-	};
+        class Military_Desert
+        {
+            name = "Military (Desert)";
+            #include "military\desert.hpp"
+        };
+        class Military_Woodland
+        {
+            name = "Military (Woodland)";
+            #include "military\woodland.hpp"
+        };
+        class Military_Pacific
+        {
+            name = "Military (Pacific)";
+            #include "military\pacific.hpp"
+        };
+    };
 };
 
