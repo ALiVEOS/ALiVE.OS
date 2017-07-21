1e634838a622e9ee201e6d08407f4f9f4cfc1afa
Marcel de Vries <marceldev89@gmail.com>
2017-07-21 19:37:09 +0200

    [compositions_a3] Merge optional APEX compositions into main
    A3 1.72 (Malden) made the Tanoa/APEX terrain assets "free"

--- addons/composition_a3/CfgGroups.hpp
+++ addons/composition_a3/CfgGroups.hpp
@@ -1,51 +1,66 @@
 class CfgGroups
 {
-	class Empty
-	{
-		side = 8;
-		name = "Compositions";
-		class Civilian
-		{
+    class Empty
+    {
+        side = 8;
+        name = "Compositions";
+        class Civilian
+        {
             #include "civilian\urban.hpp"
         };
-	    class Civilian_Desert
-	    {
-	    	name = "Civilian (Desert)";
-	    	#include <civilian\desert.hpp>
-	    };
-	    class Civilian_Woodland
-	    {
-	    	name = "Civilian (Woodland)";
-	    	#include "civilian\woodland.hpp"
-	    };
-		class Guerrilla
-		{
+        class Civilian_Desert
+        {
+            name = "Civilian (Desert)";
+            #include <civilian\desert.hpp>
+        };
+        class Civilian_Pacific
+        {
+            name = "Civilian (Pacific)";
+            #include "civilian\pacific.hpp"
+        };
+        class Civilian_Woodland
+        {
+            name = "Civilian (Woodland)";
+            #include "civilian\woodland.hpp"
+        };
+        class Guerrilla
+        {
             #include "guerrilla\urban.hpp"
         };
-	    class Guerrilla_Desert
-	    {
-	    	name = "Guerrilla (Desert)";
-	    	#include "guerrilla\desert.hpp"
-	    };
-	    class Guerrilla_Woodland
-	    {
-	    	name = "Guerrilla (Woodland)";
-	    	#include "guerrilla\woodland.hpp"
-	    };
-		class Military
-		{
+        class Guerrilla_Desert
+        {
+            name = "Guerrilla (Desert)";
+            #include "guerrilla\desert.hpp"
+        };
+        class Guerrilla_Pacific
+        {
+            name = "Guerrilla (Pacific)";
+            #include "guerrilla\pacific.hpp"
+        };
+        class Guerrilla_Woodland
+        {
+            name = "Guerrilla (Woodland)";
+            #include "guerrilla\woodland.hpp"
+        };
+        class Military
+        {
             #include "military\urban.hpp"
         };
-	    class Military_Desert
-	    {
-	    	name = "Military (Desert)";
-	    	#include "military\desert.hpp"
-	    };
-	    class Military_Woodland
-	    {
-	    	name = "Military (Woodland)";
-	    	#include "military\woodland.hpp"
-	    };
-	};
+        class Military_Desert
+        {
+            name = "Military (Desert)";
+            #include "military\desert.hpp"
+        };
+        class Military_Pacific
+        {
+            name = "Military (Pacific)";
+            #include "military\pacific.hpp"
+        };
+        class Military_Woodland
+        {
+            name = "Military (Woodland)";
+            #include "military\woodland.hpp"
+        };
+    };
 };
 
--- optional/composition_apex/config.cpp
+++
@@ -1,4 +0,0 @@
-#include <script_component.hpp>
-
-#include <CfgPatches.hpp>
-#include <CfgGroups.hpp>
