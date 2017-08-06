73b9fb7a02bebdef039262f53ae9714b14c4e611
Marcel de Vries <marceldev89@gmail.com>
2017-08-06 12:52:18 +0200

    [main] Add logistics static data for RHS (by @HeroesandvillainsOS)

---
+++ addons/main/static/logistics/RHS_SAF.hpp
@@ -0,0 +1,5 @@
+[ALIVE_factionDefaultSupports, "rhssaf_faction_un", ["rhssaf_un_ural","rhssaf_army_ural_fuel","rhssaf_army_ural","rhssaf_army_ural_open"]] call ALIVE_fnc_hashSet;
+[ALIVE_factionDefaultSupplies, "rhssaf_faction_un", ["rhs_3Ya40_1_single","rhs_7ya37_1_single","rhs_mags_crate","rhs_gear_crate","rhs_launcher_crate","rhs_weapon_crate","rhs_spec_weapons_crate"]] call ALIVE_fnc_hashSet;
+[ALIVE_factionDefaultTransport, "rhssaf_faction_un", ["rhssaf_un_ural"]] call ALIVE_fnc_hashSet;
+[ALIVE_factionDefaultAirTransport, "rhssaf_faction_un", ["rhssaf_airforce_ht48"]] call ALIVE_fnc_hashSet;
+[ALIVE_factionDefaultContainers, "rhssaf_faction_un", ["ALIVE_B_supplyCrate_F","B_CargoNet_01_ammo_F","CargoNet_01_box_F"]] call ALIVE_fnc_hashSet;
