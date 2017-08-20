2eda1f852b300d2d87011afce4058bb16deb2eb9
SpyderBlack723 <spyderblack8@gmail.com>
2017-08-20 14:04:56 -0500

    [main] Fix IFA3 logistics blocking static data from finishing

--- addons/main/static/logistics/IFA3.hpp
+++ addons/main/static/logistics/IFA3.hpp
@@ -4,20 +4,17 @@
  * If no faction specific settings are found will fall back to side
  */
 
- waitUntil {!isNil "ALiVE_STATIC_DATA_LOADED"};
-
 /*
  * Mil placement ambient vehicles per faction
  */
 
-ALIVE_factionDefaultSupports = [] call ALIVE_fnc_hashCreate;
 [ALIVE_factionDefaultSupports, "LIB_RKKA", ["LIB_Zis5v_Med","LIB_Zis6_Parm","LIB_Zis5v_Fuel","LIB_US6_Ammo","LIB_Zis5v"]] call ALIVE_fnc_hashSet;
 [ALIVE_factionDefaultSupports, "LIB_RKKA_w", ["LIB_Zis5v_w","LIB_Zis5v_med_w","LIB_Zis5v_fuel_w","LIB_Zis6_parm_w"]] call ALIVE_fnc_hashSet;
-[ALIVE_factionDefaultSupports, "LIB_NAC", ["LIB_US_NAC_GMC_Ambulance","LIB_US_NAC_GMC_Ammo","LIB_US_NAC_GMC_Fuel","LIB_US_NAC_GMC_Parm","LIB_US_NAC_Willys_MB"]] call ALIVE_fnc_hashSet; 
-[ALIVE_factionDefaultSupports, "LIB_US_ARMY", ["LIB_US_GMC_Ambulance","LIB_US_GMC_Ammo","IB_US_GMC_Fuel","LIB_US_GMC_Parm","LIB_US_Willys_MB"]] call ALIVE_fnc_hashSet; 
-[ALIVE_factionDefaultSupports, "LIB_US_RANGERS", ["LIB_US_GMC_Ambulance","LIB_US_GMC_Ammo","IB_US_GMC_Fuel","LIB_US_GMC_Parm","LIB_US_Willys_MB"]] call ALIVE_fnc_hashSet; 
-[ALIVE_factionDefaultSupports, "LIB_US_101AB", ["LIB_US_GMC_Ambulance","LIB_US_GMC_Ammo","IB_US_GMC_Fuel","LIB_US_GMC_Parm","LIB_US_Willys_MB"]] call ALIVE_fnc_hashSet; 
-[ALIVE_factionDefaultSupports, "LIB_US_82AB", ["LIB_US_GMC_Ambulance","LIB_US_GMC_Ammo","IB_US_GMC_Fuel","LIB_US_GMC_Parm","LIB_US_Willys_MB"]] call ALIVE_fnc_hashSet; 
+[ALIVE_factionDefaultSupports, "LIB_NAC", ["LIB_US_NAC_GMC_Ambulance","LIB_US_NAC_GMC_Ammo","LIB_US_NAC_GMC_Fuel","LIB_US_NAC_GMC_Parm","LIB_US_NAC_Willys_MB"]] call ALIVE_fnc_hashSet;
+[ALIVE_factionDefaultSupports, "LIB_US_ARMY", ["LIB_US_GMC_Ambulance","LIB_US_GMC_Ammo","IB_US_GMC_Fuel","LIB_US_GMC_Parm","LIB_US_Willys_MB"]] call ALIVE_fnc_hashSet;
+[ALIVE_factionDefaultSupports, "LIB_US_RANGERS", ["LIB_US_GMC_Ambulance","LIB_US_GMC_Ammo","IB_US_GMC_Fuel","LIB_US_GMC_Parm","LIB_US_Willys_MB"]] call ALIVE_fnc_hashSet;
+[ALIVE_factionDefaultSupports, "LIB_US_101AB", ["LIB_US_GMC_Ambulance","LIB_US_GMC_Ammo","IB_US_GMC_Fuel","LIB_US_GMC_Parm","LIB_US_Willys_MB"]] call ALIVE_fnc_hashSet;
+[ALIVE_factionDefaultSupports, "LIB_US_82AB", ["LIB_US_GMC_Ambulance","LIB_US_GMC_Ammo","IB_US_GMC_Fuel","LIB_US_GMC_Parm","LIB_US_Willys_MB"]] call ALIVE_fnc_hashSet;
 [ALIVE_factionDefaultSupports, "LIB_US_ARMY_w", ["LIB_US_GMC_Ambulance_w","LIB_US_GMC_Ammo_w","IB_US_GMC_Fuel_w","LIB_US_GMC_Parm_w","LIB_US_Willys_MB_w"]] call ALIVE_fnc_hashSet;
 [ALIVE_factionDefaultSupports, "LIB_WEHRMACHT", ["LIB_Kfz1","LIB_Kfz1_Hood","LIB_opelblitz_ambulance","LIB_opelblitz_ammo","LIB_opelblitz_fuel","LIB_opelblitz_parm"]] call ALIVE_fnc_hashSet;
 [ALIVE_factionDefaultSupports, "LIB_WEHRMACHT_w", ["LIB_Kfz1_w","LIB_Kfz1_Hood_w","LIB_opelblitz_ambulance","LIB_opelblitz_ammo","LIB_opelblitz_fuel","LIB_opelblitz_parm"]] call ALIVE_fnc_hashSet;
@@ -28,7 +25,6 @@ ALIVE_factionDefaultSupports = [] call ALIVE_fnc_hashCreate;
  * Mil placement random supply boxes per faction
  */
 
-ALIVE_factionDefaultSupplies = [] call ALIVE_fnc_hashCreate;
 [ALIVE_factionDefaultSupplies, "LIB_RKKA", ["LIB_BasicAmmunitionBox_SU","LIB_BasicWeaponsBox_SU","LIB_AmmoCrate_Mortar_SU","LIB_WeaponsBox_Big_SU","LIB_Lone_Big_Box","LIB_AmmoCrate_Arty_SU"]] call ALIVE_fnc_hashSet;
 [ALIVE_factionDefaultSupplies, "LIB_RKKA_w", ["LIB_BasicAmmunitionBox_SU","LIB_BasicWeaponsBox_SU","LIB_AmmoCrate_Mortar_SU","LIB_WeaponsBox_Big_SU","LIB_Lone_Big_Box","LIB_AmmoCrate_Arty_SU"]] call ALIVE_fnc_hashSet;
 [ALIVE_factionDefaultSupplies, "LIB_NAC", ["LIB_BasicAmmunitionBox_US","LIB_BasicWeaponsBox_US"]] call ALIVE_fnc_hashSet;
@@ -46,7 +42,6 @@ ALIVE_factionDefaultSupplies = [] call ALIVE_fnc_hashCreate;
  * Mil logistics convoy transport vehicles per faction
  */
 
-ALIVE_factionDefaultTransport = [] call ALIVE_fnc_hashCreate;
 [ALIVE_factionDefaultTransport, "LIB_RKKA", ["LIB_US6_Tent","LIB_US6_Open","LIB_Zis5v"]] call ALIVE_fnc_hashSet;
 [ALIVE_factionDefaultTransport, "LIB_RKKA_w", ["LIB_Us6_tent_w","LIB_Zis5v_w"]] call ALIVE_fnc_hashSet;
 [ALIVE_factionDefaultTransport, "LIB_NAC", ["LIB_US_NAC_GMC_Tent","LIB_US_NAC_GMC_Open"]] call ALIVE_fnc_hashSet;
@@ -64,7 +59,6 @@ ALIVE_factionDefaultTransport = [] call ALIVE_fnc_hashCreate;
  * Mil logistics air transport vehicles per faction
  */
 
-ALIVE_factionDefaultAirTransport = [] call ALIVE_fnc_hashCreate;
 [ALIVE_factionDefaultAirTransport, "LIB_RKKA", []] call ALIVE_fnc_hashSet;
 [ALIVE_factionDefaultAirTransport, "LIB_RKKA_w", []] call ALIVE_fnc_hashSet;
 [ALIVE_factionDefaultAirTransport, "LIB_NAC", []] call ALIVE_fnc_hashSet;
@@ -82,7 +76,6 @@ ALIVE_factionDefaultAirTransport = [] call ALIVE_fnc_hashCreate;
  * Mil logistics airdrop containers per faction
  */
 
-ALIVE_factionDefaultContainers = [] call ALIVE_fnc_hashCreate;
 [ALIVE_factionDefaultContainers, "LIB_RKKA", ["ALIVE_O_supplyCrate_F"]] call ALIVE_fnc_hashSet;
 [ALIVE_factionDefaultContainers, "LIB_RKKA_w", ["ALIVE_O_supplyCrate_F"]] call ALIVE_fnc_hashSet;
 [ALIVE_factionDefaultContainers, "LIB_NAC", ["ALIVE_I_supplyCrate_F"]] call ALIVE_fnc_hashSet;
