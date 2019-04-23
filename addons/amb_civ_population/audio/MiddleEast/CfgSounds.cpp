cc9fb54f6066f8be4ba735dbcd965c44bc5dde36
Tupolov <tupolov73@gmail.com>
2019-04-23 22:53:49 +0100

    [amb civ] Add middle eastern environment sounds. Add civ pop support for middle eastern CUP and CFP civilian factions.

---
+++ addons/amb_civ_population/audio/MiddleEast/CfgSounds.cpp
@@ -0,0 +1,104 @@
+    class ALiVE_Civpop_Audio_ME_1
+    {
+        name = "sfxsound";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\MiddleEast\sfxsound.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+ 
+    class ALiVE_Civpop_Audio_ME_2
+    {
+        name = "sfxsound2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\MiddleEast\sfxsound2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+
+    class ALiVE_Civpop_Audio_ME_3
+    {
+        name = "sfxsound3";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\MiddleEast\sfxsound3.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+
+    class ALiVE_Civpop_Audio_ME_4
+    {
+        name = "sfxsound4";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\MiddleEast\sfxsound4.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+
+    class ALiVE_Civpop_Audio_ME_5
+    {
+        name = "sfxsound5";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\MiddleEast\sfxsound5.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    
+    class ALiVE_Civpop_Audio_ME_6
+    {
+        name = "sfxsound2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\MiddleEast\sfxsound2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+
+    class ALiVE_Civpop_Audio_ME_7
+    {
+        name = "sfxsound3";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\MiddleEast\sfxsound3.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+
+    class ALiVE_Civpop_Audio_ME_8
+    {
+        name = "sfxsound4";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\MiddleEast\sfxsound4.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+        
+    class ALiVE_Civpop_Audio_ME_9
+    {
+        name = "music1";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\MiddleEast\music1.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+
+    class ALiVE_Civpop_Audio_ME_10
+    {
+        name = "music2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\MiddleEast\music2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+
+    class ALiVE_Civpop_Audio_ME_11
+    {
+        name = "prayer2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\MiddleEast\prayer2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+
+    class ALiVE_Civpop_Audio_ME_12
+    {
+        name = "ISISpropoganda";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\MiddleEast\ISISpropoganda.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+
+    class ALiVE_Civpop_Audio_ME_13
+    {
+        name = "arab_talking";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\MiddleEast\arab_talking.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+
+    class ALiVE_Civpop_Audio_ME_14
+    {
+        name = "arabicsong1";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\MiddleEast\arabicsong1.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+        
+    class ALiVE_Civpop_Audio_ME_15
+    {
+        name = "arabicsong2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\MiddleEast\arabicsong2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
