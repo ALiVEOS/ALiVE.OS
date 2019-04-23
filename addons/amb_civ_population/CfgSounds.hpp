39266c2eac0824ed2790fc8f38256b3c0fdf45a2
Tupolov <tupolov73@gmail.com>
2019-04-23 22:53:49 +0100

    [amb civ] Add middle eastern environment sounds. Add civ pop support for middle eastern CUP and CFP civilian factions.

--- addons/amb_civ_population/CfgSounds.hpp
+++ addons/amb_civ_population/CfgSounds.hpp
@@ -1,125 +1,130 @@
 class CfgSounds
 {
     sounds[] = {};
-    class Track1
+    class ALiVE_Civpop_Audio_1
     {
-        name = "Track1";
+        name = "track1";
         sound[] = {"x\alive\addons\amb_civ_population\audio\track1.ogg", 1, 1, 30};
         titles[] = {};
     };
-    class Track2
+    class ALiVE_Civpop_Audio_2
     {
-        name = "Track2";
+        name = "track2";
         sound[] = {"x\alive\addons\amb_civ_population\audio\track2.ogg", 1, 1, 30};
         titles[] = {};
     };
-    class Track3
+    class ALiVE_Civpop_Audio_3
     {
-        name = "Track3";
+        name = "track3";
         sound[] = {"x\alive\addons\amb_civ_population\audio\track3.ogg", 1, 1, 30};
         titles[] = {};
     };
-    class Track4
+    class ALiVE_Civpop_Audio_4
     {
-        name = "Track4";
+        name = "track4";
         sound[] = {"x\alive\addons\amb_civ_population\audio\track4.ogg", 1, 1, 30};
         titles[] = {};
     };
-    class Track5
+    class ALiVE_Civpop_Audio_5
     {
-        name = "Track5";
+        name = "track5";
         sound[] = {"x\alive\addons\amb_civ_population\audio\track5.ogg", 1, 1, 30};
         titles[] = {};
     };
-    class Track6
+    class ALiVE_Civpop_Audio_6
     {
-        name = "Track6";
+        name = "track6";
         sound[] = {"x\alive\addons\amb_civ_population\audio\track6.ogg", 1, 1, 30};
         titles[] = {};
     };
-    class Track7
+    class ALiVE_Civpop_Audio_7
     {
-        name = "Track7";
+        name = "radio1";
         sound[] = {"x\alive\addons\amb_civ_population\audio\radio1.ogg", 1, 1, 30};
         titles[] = {};
     };
-    class Track8
+    class ALiVE_Civpop_Audio_8
     {
-        name = "Track8";
+        name = "radio2";
         sound[] = {"x\alive\addons\amb_civ_population\audio\radio2.ogg", 1, 1, 30};
         titles[] = {};
     };
-    class Track9
+    class ALiVE_Civpop_Audio_9
     {
-        name = "Track9";
+        name = "radio3";
         sound[] = {"x\alive\addons\amb_civ_population\audio\radio3.ogg", 1, 1, 30};
         titles[] = {};
     };
-    class Track10
+    class ALiVE_Civpop_Audio_10
     {
-        name = "Track10";
+        name = "radio4";
         sound[] = {"x\alive\addons\amb_civ_population\audio\radio4.ogg", 1, 1, 30};
         titles[] = {};
     };
-    class Track11
+    class ALiVE_Civpop_Audio_11
     {
-        name = "Track11";
+        name = "radio5";
         sound[] = {"x\alive\addons\amb_civ_population\audio\radio5.ogg", 1, 1, 30};
         titles[] = {};
     };
-    class Track12
+    class ALiVE_Civpop_Audio_12
     {
-        name = "Track12";
+        name = "radio6";
         sound[] = {"x\alive\addons\amb_civ_population\audio\radio6.ogg", 1, 1, 30};
         titles[] = {};
     };
-    class Track13
+    class ALiVE_Civpop_Audio_13
     {
-        name = "Track13";
+        name = "snoring";
         sound[] = {"x\alive\addons\amb_civ_population\audio\snoring.ogg", 0.7, 1, 15};
         titles[] = {};
     };
-    class Track14
+    class ALiVE_Civpop_Audio_14
     {
-        name = "Track14";
+        name = "sex";
         sound[] = {"x\alive\addons\amb_civ_population\audio\sex.ogg", 0.7, 1, 20};
         titles[] = {};
     };
-    class Track15
+    class ALiVE_Civpop_Audio_15
     {
-        name = "Track15";
+        name = "crying";
         sound[] = {"x\alive\addons\amb_civ_population\audio\crying.ogg", 1, 1, 20};
         titles[] = {};
     };
-    class Track16
+    class ALiVE_Civpop_Audio_16
     {
-        name = "Track16";
+        name = "caralarm";
         sound[] = {"x\alive\addons\amb_civ_population\audio\caralarm.ogg", 1, 1, 15};
         titles[] = {};
     };
-    class Track17
+    class ALiVE_Civpop_Audio_17
     {
-        name = "Track17";
+        name = "baby";
         sound[] = {"x\alive\addons\amb_civ_population\audio\baby.ogg", 1, 1, 20};
         titles[] = {};
     };
-    class Track18
+    class ALiVE_Civpop_Audio_18
     {
-        name = "Track18";
+        name = "smalldog";
         sound[] = {"x\alive\addons\amb_civ_population\audio\smalldog.ogg", 1, 1, 30};
         titles[] = {};
     };
-    class Track19
+    class ALiVE_Civpop_Audio_19
     {
-        name = "Track19";
+        name = "largedog";
         sound[] = {"x\alive\addons\amb_civ_population\audio\largedog.ogg", 1, 1, 30};
         titles[] = {};
     };
-    class Track20
+    class ALiVE_Civpop_Audio_20
     {
-        name = "Track20";
+        name = "meddog";
         sound[] = {"x\alive\addons\amb_civ_population\audio\meddog.ogg", 1, 1, 30};
         titles[] = {};
     };
+
+    //#include "audio\African\CfgSounds.cpp"
+    #include "audio\MiddleEast\CfgSounds.cpp"
+    //#include "audio\WW2\CfgSounds.cpp"
+    //#include "audio\Pacfic\CfgSounds.cpp"
 
 };
--- addons/main/static/CivPop.hpp
+++ addons/main/static/CivPop.hpp
@@ -3,44 +3,83 @@
  */
 
 ALIVE_civilianWeapons = [] call ALIVE_fnc_hashCreate;
+
+// Vanilla
 [ALIVE_civilianWeapons, "CIV", [["hgun_Pistol_heavy_01_F","11Rnd_45ACP_Mag"],["hgun_PDW2000_F","30Rnd_9x21_Mag"],["SMG_02_ARCO_pointg_F","30Rnd_9x21_Mag"],["arifle_TRG21_F","30Rnd_556x45_Stanag"]]] call ALIVE_fnc_hashSet;
 [ALIVE_civilianWeapons, "CIV_F", [["hgun_Pistol_heavy_01_F","11Rnd_45ACP_Mag"],["hgun_PDW2000_F","30Rnd_9x21_Mag"],["SMG_02_ARCO_pointg_F","30Rnd_9x21_Mag"],["arifle_TRG21_F","30Rnd_556x45_Stanag"]]] call ALIVE_fnc_hashSet;
+
+// Custom
 [ALIVE_civilianWeapons, "mas_afr_civ", [["arifle_mas_ak_74m","30Rnd_mas_545x39_mag"],["arifle_mas_aks74u","30Rnd_mas_545x39_mag"],["arifle_mas_akm","30Rnd_mas_762x39_mag"]]] call ALIVE_fnc_hashSet;
 [ALIVE_civilianWeapons, "caf_ag_afr_civ", [["caf_AK47","CAF_30Rnd_762x39_AK"],["caf_AK74","CAF_30Rnd_545x39_AK"]]] call ALIVE_fnc_hashSet;
 [ALIVE_civilianWeapons, "caf_ag_me_civ", [["caf_AK47","CAF_30Rnd_762x39_AK"],["caf_AK74","CAF_30Rnd_545x39_AK"]]] call ALIVE_fnc_hashSet;
 [ALIVE_civilianWeapons, "drirregularsC", [["arifle_mas_ak_74m","30Rnd_mas_545x39_mag"],["arifle_mas_aks74u","30Rnd_mas_545x39_mag"],["arifle_mas_akm","30Rnd_mas_762x39_mag"]]] call ALIVE_fnc_hashSet;
 
+// CUP
 [ALIVE_civilianWeapons, "CUP_C_TK", [["CUP_arifle_ak47","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_ak74","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKM","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKS","CUP_30Rnd_762x39_AK47_M"],["CUP_srifle_LeeEnfield","CUP_10x_303_M"],["CUP_hgun_Makarov","CUP_8Rnd_9x18_Makarov_M"]]] call ALIVE_fnc_hashSet;
 
+[ALIVE_civilianWeapons, "CUP_C_CHERNARUS", [["CUP_arifle_ak47","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_ak74","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKM","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKS","CUP_30Rnd_762x39_AK47_M"],["CUP_srifle_LeeEnfield","CUP_10x_303_M"],["CUP_hgun_Makarov","CUP_8Rnd_9x18_Makarov_M"]]] call ALIVE_fnc_hashSet;
+
+[ALIVE_civilianWeapons, "CUP_C_RU", [["CUP_arifle_ak47","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_ak74","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKM","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKS","CUP_30Rnd_762x39_AK47_M"],["CUP_srifle_LeeEnfield","CUP_10x_303_M"],["CUP_hgun_Makarov","CUP_8Rnd_9x18_Makarov_M"]]] call ALIVE_fnc_hashSet;
+
+// CFP
 [ALIVE_civilianWeapons, "CFP_C_ME", [["CUP_arifle_ak47","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_ak74","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKM","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKS","CUP_30Rnd_762x39_AK47_M"],["CUP_srifle_LeeEnfield","CUP_10x_303_M"],["CUP_hgun_Makarov","CUP_8Rnd_9x18_Makarov_M"]]] call ALIVE_fnc_hashSet;
 
 [ALIVE_civilianWeapons, "CFP_C_AFRISLAMIC", [["CUP_arifle_ak47","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_ak74","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKM","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKS","CUP_30Rnd_762x39_AK47_M"],["CUP_srifle_LeeEnfield","CUP_10x_303_M"],["CUP_hgun_Makarov","CUP_8Rnd_9x18_Makarov_M"]]] call ALIVE_fnc_hashSet;
 
 [ALIVE_civilianWeapons, "CFP_C_AFRCHRISTIAN", [["CUP_arifle_ak47","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_ak74","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKM","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKS","CUP_30Rnd_762x39_AK47_M"],["CUP_srifle_LeeEnfield","CUP_10x_303_M"],["CUP_hgun_Makarov","CUP_8Rnd_9x18_Makarov_M"]]] call ALIVE_fnc_hashSet;
 
+[ALIVE_civilianWeapons, "CFP_C_ASIA", [["CUP_arifle_ak47","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_ak74","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKM","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKS","CUP_30Rnd_762x39_AK47_M"],["CUP_srifle_LeeEnfield","CUP_10x_303_M"],["CUP_hgun_Makarov","CUP_8Rnd_9x18_Makarov_M"]]] call ALIVE_fnc_hashSet;
+
 [ALIVE_civilianWeapons, "CFP_C_AFG", [["CUP_arifle_ak47","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_ak74","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKM","CUP_30Rnd_762x39_AK47_M"],["CUP_arifle_AKS","CUP_30Rnd_762x39_AK47_M"],["CUP_srifle_LeeEnfield","CUP_10x_303_M"],["CUP_hgun_Makarov","CUP_8Rnd_9x18_Makarov_M"]]] call ALIVE_fnc_hashSet;
 
+// Civ Pop Env sounds default
 ALIVE_civilianHouseTracks = [] call ALIVE_fnc_hashCreate;
-[ALIVE_civilianHouseTracks, "Track1", 180] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track2", 188] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track3", 199] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track4", 246] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track5", 335] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track6", 199] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track7", 177] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track8", 235] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track9", 246] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track10", 292] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track11", 189] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track12", 203] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track13", 16] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track14", 128] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track15", 14] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track16", 7] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track17", 19] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track18", 4] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track19", 22] call ALIVE_fnc_hashSet;
-[ALIVE_civilianHouseTracks, "Track20", 2] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_1", 180] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_2", 188] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_3", 199] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_4", 246] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_5", 335] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_6", 199] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_7", 177] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_8", 235] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_9", 246] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_10", 292] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_11", 189] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_12", 203] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_13", 16] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_14", 128] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_15", 14] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_16", 7] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_17", 19] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_18", 4] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_19", 22] call ALIVE_fnc_hashSet;
+[ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_20", 2] call ALIVE_fnc_hashSet;
 
+// Civ Pop Env sounds by faction
+ALIVE_civilianFactionHouseTracks = [] call ALIVE_fnc_hashCreate;
 
+CUP_C_TK_ALIVE_civilianHouseTracks = +ALIVE_civilianHouseTracks;
+[CUP_C_TK_ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_ME_1", 135] call ALIVE_fnc_hashSet;
+[CUP_C_TK_ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_ME_2", 85] call ALIVE_fnc_hashSet;
+[CUP_C_TK_ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_ME_3", 35] call ALIVE_fnc_hashSet;
+[CUP_C_TK_ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_ME_4", 55] call ALIVE_fnc_hashSet;
+[CUP_C_TK_ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_ME_5", 45] call ALIVE_fnc_hashSet;
+[CUP_C_TK_ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_ME_6", 50] call ALIVE_fnc_hashSet;
+[CUP_C_TK_ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_ME_7", 50] call ALIVE_fnc_hashSet;
+[CUP_C_TK_ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_ME_8", 85] call ALIVE_fnc_hashSet;
+[CUP_C_TK_ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_ME_9", 140] call ALIVE_fnc_hashSet;
+[CUP_C_TK_ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_ME_10", 125] call ALIVE_fnc_hashSet;
+[CUP_C_TK_ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_ME_11", 95] call ALIVE_fnc_hashSet;
+[CUP_C_TK_ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_ME_12", 70] call ALIVE_fnc_hashSet;
+[CUP_C_TK_ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_ME_13", 21] call ALIVE_fnc_hashSet;
+[CUP_C_TK_ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_ME_14", 280] call ALIVE_fnc_hashSet;
+[CUP_C_TK_ALIVE_civilianHouseTracks, "ALiVE_Civpop_Audio_ME_15", 335] call ALIVE_fnc_hashSet;
+
+[ALIVE_civilianFactionHouseTracks, "CUP_C_TK", CUP_C_TK_ALIVE_civilianHouseTracks] call ALIVE_fnc_hashSet;
+
+CFP_C_AFG_ALIVE_civilianHouseTracks = +CUP_C_TK_ALIVE_civilianHouseTracks;
+[ALIVE_civilianFactionHouseTracks, "CFP_C_AFG", CFP_C_AFG_ALIVE_civilianHouseTracks] call ALIVE_fnc_hashSet;
+
+CFP_C_ME_ALIVE_civilianHouseTracks = +CUP_C_TK_ALIVE_civilianHouseTracks;
+[ALIVE_civilianFactionHouseTracks, "CFP_C_ME", CFP_C_ME_ALIVE_civilianHouseTracks] call ALIVE_fnc_hashSet;
 
