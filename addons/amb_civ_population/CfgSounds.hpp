32a070e04cb82a0299b4602ce4f35881aff84602
Tupolov <tupolov73@gmail.com>
2019-04-24 16:53:07 +0100

    [amb civs] Add more animal sounds to various environments.

--- addons/amb_civ_population/CfgSounds.hpp
+++ addons/amb_civ_population/CfgSounds.hpp
@@ -82,7 +82,7 @@ class CfgSounds
     class ALiVE_Civpop_Audio_14
     {
         name = "sex";
-        sound[] = {"x\alive\addons\amb_civ_population\audio\sex.ogg", 0.7, 1, 20};
+        sound[] = {"x\alive\addons\amb_civ_population\audio\sex.ogg", 0.7, 1, 10};
         titles[] = {};
     };
     class ALiVE_Civpop_Audio_15
@@ -94,7 +94,7 @@ class CfgSounds
     class ALiVE_Civpop_Audio_16
     {
         name = "caralarm";
-        sound[] = {"x\alive\addons\amb_civ_population\audio\caralarm.ogg", 1, 1, 15};
+        sound[] = {"x\alive\addons\amb_civ_population\audio\caralarm.ogg", 1, 1, 30};
         titles[] = {};
     };
     class ALiVE_Civpop_Audio_17
@@ -115,7 +115,129 @@ class CfgSounds
         sound[] = {"x\alive\addons\amb_civ_population\audio\largedog.ogg", 1, 1, 30};
         titles[] = {};
     };
+
     class ALiVE_Civpop_Audio_20
+    {
+        name = "dog1";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog1.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+
+    class ALiVE_Civpop_Audio_21
+    {
+        name = "dog2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_22
+    {
+        name = "dog3";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog3.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_23
+    {
+        name = "dog4";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog4.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_24
+    {
+        name = "dog5";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog5.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_25
+    {
+        name = "dog6";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog6.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_26
+    {
+        name = "dog7";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog7.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_27
+    {
+        name = "dog8";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog8.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_28
+    {
+        name = "dog9";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog9.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_29
+    {
+        name = "dog10";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog10.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_30
+    {
+        name = "dog11";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog11.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_31
+    {
+        name = "dog12";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog12.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_32
+    {
+        name = "dog13";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog13.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_33
+    {
+        name = "dog14";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog14.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_34
+    {
+        name = "dog15";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog15.ogg", 1, 1, 20};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_35
+    {
+        name = "dog16";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog16.ogg", 1, 1, 20};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_36
+    {
+        name = "dog17";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog17.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_37
+    {
+        name = "dog18";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog18.ogg", 1, 1, 20};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_38
+    {
+        name = "dog19";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog19.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_39
+    {
+        name = "dog20";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\dog20.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_40
     {
         name = "meddog";
         sound[] = {"x\alive\addons\amb_civ_population\audio\meddog.ogg", 1, 1, 30};
