b4bda78c3a010f8ca53ef574bca1d6094bde18f9
Tupolov <tupolov73@gmail.com>
2019-05-06 23:05:17 +0100

    [amb civ] Fix issue where African sounds were not playing, added more African sounds

--- addons/amb_civ_population/audio/African/CfgSounds.cpp
+++ addons/amb_civ_population/audio/African/CfgSounds.cpp
@@ -82,3 +82,197 @@
         sound[] = {"x\alive\addons\amb_civ_population\audio\African\africansong3.ogg", 1, 1, 30};
         titles[] = {};
     };
+
+    class ALiVE_Civpop_Audio_AFR_13
+    {
+        name = "afrcrowdmed";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\afrcrowdmed.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_14
+    {
+        name = "afrcrowdsmall";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\afrcrowdsmall.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_15
+    {
+        name = "afrgrouptalk";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\afrgrouptalk.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_16
+    {
+        name = "afrgrouptalk2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\afrgrouptalk2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_17
+    {
+        name = "africancity";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\africancity.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_18
+    {
+        name = "africancity2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\africancity2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_19
+    {
+        name = "africancity3";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\africancity3.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_20
+    {
+        name = "africancity4";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\africancity4.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_21
+    {
+        name = "africancity5";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\africancity5.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_22
+    {
+        name = "africancity6";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\africancity6.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_23
+    {
+        name = "africansinging";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\africansinging.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_24
+    {
+        name = "africansong1";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\africansong1.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_25
+    {
+        name = "africansong2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\africansong2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_26
+    {
+        name = "africansong3";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\africansong3.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_27
+    {
+        name = "afrmarketstreetseller";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\afrmarketstreetseller.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_28
+    {
+        name = "afrmarketstreetseller2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\afrmarketstreetseller2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_29
+    {
+        name = "afrnightsound";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\afrnightsound.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_30
+    {
+        name = "afrnightsound2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\afrnightsound2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_31
+    {
+        name = "afrnightsound3";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\afrnightsound3.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_32
+    {
+        name = "afrvillage1";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\afrvillage1.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_33
+    {
+        name = "afrvillage2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\afrvillage2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_34
+    {
+        name = "afrvillage3";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\afrvillage3.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_35
+    {
+        name = "afrvillage4";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\afrvillage4.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_36
+    {
+        name = "afrvillage5";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\afrvillage5.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_37
+    {
+        name = "afrvillage6";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\afrvillage6.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_38
+    {
+        name = "drums";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\drums.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_39
+    {
+        name = "drums2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\drums2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_40
+    {
+        name = "FreeMarket1";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\FreeMarket1.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_41
+    {
+        name = "FreeMarket2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\FreeMarket2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_42
+    {
+        name = "FreeMarket3";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\FreeMarket3.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+
+    class ALiVE_Civpop_Audio_AFR_43
+    {
+        name = "Rwanda1";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\Rwanda1.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_AFR_44
+    {
+        name = "Rwanda2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\African\Rwanda2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
