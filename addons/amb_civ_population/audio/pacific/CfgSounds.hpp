fd96dc59b6ae63864c07a660ca6f71e440dec0bf
Tupolov <tupolov73@gmail.com>
2019-05-13 23:21:10 +0100

    [amb civs] Add civ crowd support for Tanoa and pacific island environments

---
+++ addons/amb_civ_population/audio/pacific/CfgSounds.hpp
@@ -0,0 +1,78 @@
+    class ALiVE_Civpop_Audio_PAC_1
+    {
+        name = "crowdtalk1";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\pacific\crowdtalk1.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_PAC_2
+    {
+        name = "crowdtalk2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\pacific\crowdtalk2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_PAC_3
+    {
+        name = "crowdtalk3";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\pacific\crowdtalk3.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_PAC_4
+    {
+        name = "crowdtalk4";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\pacific\crowdtalk4.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_PAC_5
+    {
+        name = "crowdtalk5";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\pacific\crowdtalk5.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_PAC_6
+    {
+        name = "frenchradionews1";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\pacific\frenchradionews1.ogg", 0.4, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_PAC_7
+    {
+        name = "frenchradionews2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\pacific\frenchradionews2.ogg", 0.4, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_PAC_8
+    {
+        name = "streetcrowd1";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\pacific\streetcrowd1.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_PAC_9
+    {
+        name = "streetcrowd2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\pacific\streetcrowd2.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_PAC_10
+    {
+        name = "streetcrowd3";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\pacific\streetcrowd3.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_PAC_11
+    {
+        name = "streetcrowd4";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\pacific\streetcrowd4.ogg", 1, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_PAC_12
+    {
+        name = "tanoamusic1";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\pacific\tanoamusic1.ogg", 0.6, 1, 30};
+        titles[] = {};
+    };
+    class ALiVE_Civpop_Audio_PAC_13
+    {
+        name = "tanoamusic2";
+        sound[] = {"x\alive\addons\amb_civ_population\audio\pacific\tanoamusic2.ogg", 0.6, 1, 30};
+        titles[] = {};
+    };
