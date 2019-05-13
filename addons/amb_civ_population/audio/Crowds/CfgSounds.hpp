436044bfdcadbebdf216404a6b8a072590ffe553
Tupolov <tupolov73@gmail.com>
2019-05-13 11:33:49 +0100

    [amb civ] Tweak crowd sounds

--- addons/amb_civ_population/audio/Crowds/CfgSounds.hpp
+++ addons/amb_civ_population/audio/Crowds/CfgSounds.hpp
@@ -1,79 +1,79 @@
     class ALiVE_Civpop_Audio_Crowds_Generic_Angry_Large
     {
         name = "Generic Angry Large";
-        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\generic_angry_large.ogg", 1, 1, 30};
+        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\generic_angry_large.ogg", 1, 1, 50};
         titles[] = {};
     };
     class ALiVE_Civpop_Audio_Crowds_Generic_Angry_Med
     {
         name = "Generic Angry Med";
-        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\generic_angry_med.ogg", 1, 1, 30};
+        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\generic_angry_med.ogg", 1, 1, 50};
         titles[] = {};
     };
     class ALiVE_Civpop_Audio_Crowds_Generic_Chill_Large
     {
         name = "Generic Chill Large";
-        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\generic_chill_large.ogg", 1, 1, 30};
+        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\generic_chill_large.ogg", 1, 1, 50};
         titles[] = {};
     };
     class ALiVE_Civpop_Audio_Crowds_Generic_Chill_Med
     {
         name = "Generic Chill Med";
-        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\generic_chill_med.ogg", 1, 1, 30};
+        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\generic_chill_med.ogg", 1, 1, 50};
         titles[] = {};
     };
     class ALiVE_Civpop_Audio_Crowds_Generic_Happy_Large
     {
         name = "Generic Happy Large";
-        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\generic_happy_large.ogg", 1, 1, 30};
+        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\generic_happy_large.ogg", 1, 1, 50};
         titles[] = {};
     };
     class ALiVE_Civpop_Audio_Crowds_ME_Angry_Med
     {
         name = "ME Angry Med";
-        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\me_angry_med.ogg", 1, 1, 30};
+        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\me_angry_med.ogg", 1, 1, 50};
         titles[] = {};
     };
     class ALiVE_Civpop_Audio_Crowds_ME_Angry_Med2
     {
         name = "ME Angry Med2";
-        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\me_angry_med2.ogg", 1, 1, 30};
+        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\me_angry_med2.ogg", 1, 1, 50};
         titles[] = {};
     };
     class ALiVE_Civpop_Audio_Crowds_ME_Angry_Small
     {
         name = "ME Angry Small";
-        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\me_angry_small.ogg", 1, 1, 30};
+        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\me_angry_small.ogg", 1, 1, 50};
         titles[] = {};
     };
     class ALiVE_Civpop_Audio_Crowds_ME_Chill_Large
     {
         name = "ME Chill Large";
-        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\me_chill_large.ogg", 1, 1, 30};
+        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\me_chill_large.ogg", 1, 1, 50};
         titles[] = {};
     };
     class ALiVE_Civpop_Audio_Crowds_ME_Chill_Med
     {
         name = "ME Chill Med";
-        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\me_chill_med.ogg", 1, 1, 30};
+        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\me_chill_med.ogg", 1, 1, 50};
         titles[] = {};
     };
     class ALiVE_Civpop_Audio_Crowds_ME_Chill_Med2
     {
         name = "ME Chill Med2";
-        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\me_chill_med2.ogg", 1, 1, 30};
+        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\me_chill_med2.ogg", 1, 1, 50};
         titles[] = {};
     };
     class ALiVE_Civpop_Audio_Crowds_ME_Chill_Med3
     {
         name = "ME Chill Med3";
-        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\me_chill_med3.ogg", 1, 1, 30};
+        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\me_chill_med3.ogg", 1, 1, 50};
         titles[] = {};
     };
     class ALiVE_Civpop_Audio_Crowds_Generic_Angry_Large2
     {
         name = "Generic Angry Large2";
-        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\generic_angry_large2.ogg", 1, 1, 30};
+        sound[] = {"x\alive\addons\amb_civ_population\audio\crowds\generic_angry_large2.ogg", 1, 1, 50};
         titles[] = {};
     };
 
