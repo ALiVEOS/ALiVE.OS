1f02ad8b4615fd28c3f6b01e12119e2fc7f32a1f
Darrell Martin <darrell@briarmedia.biz>
2019-05-28 09:41:39 +0100

    [ALL] Updated version to 1.11.2.1905281

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -4,9 +4,9 @@
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
 #define MAJOR 1
 #define MINOR 11
-#define PATCHLVL 1
+#define PATCHLVL 2
 // BUILD should be YYMMDDR eg 1311211
-#define BUILD 1905231
+#define BUILD 1905281
 
 #define CLUSTERBUILD "Arma 3","Arma3",192,145639,"Stable"
 
