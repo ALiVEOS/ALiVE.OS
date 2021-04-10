be86a1adbe348280651524ea7bee2f7d39a8c0c3
Darrell Martin <darrell@briarmedia.biz>
2021-04-10 17:26:11 +0100

    [ALL] Updated version to 1.13.8.2104101 for S.O.G Prairie Fire Closed Beta - clusterbuild 2.04.147515 - creatordlcbeta branch (base game build 2.04)

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -4,9 +4,9 @@
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
 #define MAJOR 1
 #define MINOR 13
-#define PATCHLVL 7
+#define PATCHLVL 8
 // BUILD should be YYMMDDR eg 1311211
-#define BUILD 2104081
+#define BUILD 2104101
 
 #define CLUSTERBUILD "Arma 3","Arma3",204,147515,"Stable"
 
