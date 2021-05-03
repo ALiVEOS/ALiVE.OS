84ea690ee8d70c150ff891ec43f1f96a1270509e
Davis Brown <spyderblack8@gmail.com>
2021-05-03 00:55:06 -0500

    [sys_statistics] Fix missing CBA EH's on module

--- addons/sys_statistics/CfgVehicles.hpp
+++ addons/sys_statistics/CfgVehicles.hpp
@@ -1,28 +1,25 @@
 class CfgVehicles {
-        class ModuleAliveBase;
-        class ADDON : ModuleAliveBase
-        {
-                scope = 1;
-                displayName = "$STR_ALIVE_DISABLE_STATISTICS";
-                isGlobal = 2;
-                functionPriority = 32;
-
+    class ModuleAliveBase;
+    class ModuleAliveBase2: ModuleAliveBase {
+        class EventHandlers;
+    };
+    class ADDON : ModuleAliveBase2 {
+        scope = 1;
+        displayName = "$STR_ALIVE_DISABLE_STATISTICS";
+        isGlobal = 2;
+        functionPriority = 32;
         icon = "x\alive\addons\sys_statistics\icon_sys_statistics.paa";
         picture = "x\alive\addons\sys_statistics\icon_sys_statistics.paa";
-                class Arguments
-                {
-                        class Condition
-                        {
-                                displayName = "Condition:";
-                                description = "";
-                                defaultValue = "true";
-                        };
-                };
-
-                class Eventhandlers
-                {
-                    init = "call ALIVE_fnc_statisticsDisable;";
-                };
-
+        class Arguments {
+            class Condition {
+                displayName = "Condition:";
+                description = "";
+                defaultValue = "true";
+            };
         };
+
+        class Eventhandlers : Eventhandlers{
+            init = "call ALIVE_fnc_statisticsDisable;";
+        };
+    };
 };
