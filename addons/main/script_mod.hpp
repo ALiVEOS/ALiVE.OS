55e1a34372b7ebb2b108c446ea316331a0c841d2
Tupolov <tupolov73@gmail.com>
2021-05-06 13:29:50 +0100

    [mai] Update mod version

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -3,10 +3,10 @@
 
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
 #define MAJOR 1
-#define MINOR 13
-#define PATCHLVL 9
+#define MINOR 14
+#define PATCHLVL 1
 // BUILD should be YYMMDDR eg 1311211
-#define BUILD 2105031
+#define BUILD 2105061
 
 #define CLUSTERBUILD "Arma 3","Arma3",204,147540,"Stable"
 
