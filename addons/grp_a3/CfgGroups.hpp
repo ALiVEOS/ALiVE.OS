a52149d01469c27e0604b076a8de27bcf9ad7f89
Marcel de Vries <marceldev89@gmail.com>
2021-05-07 23:58:05 +0200

    Removed commented CfgGroups changes

--- addons/grp_a3/CfgGroups.hpp
+++ addons/grp_a3/CfgGroups.hpp
@@ -2501,81 +2501,7 @@ class CfgGroups
                     };
                 };
             };
-        }; /*
-        class VN_PAVN
-        {
-            class Naval
-            {
-                name = "Single Boat Groups";
-                class vn_o_group_boat_03
-                {
-                    name = $STR_VN_O_GROUP_BOAT_NVA_01;
-                    faction = "O_PAVN";
-                    icon = "\A3\ui_f\data\map\markers\nato\o_naval.paa";
-                    rarityGroup = 0.5;
-                    side = 0;
-                    class Unit0
-                    {
-                        side = 0;
-                        vehicle = "vn_o_boat_03_02";
-                        rank = "CAPTAIN";
-                        position[] = {0,0,0};
-                    };
-                };
-                class vn_o_group_boat_04
-                {
-                    name = $STR_VN_O_GROUP_BOAT_NVA_01;
-                    faction = "O_PAVN";
-                    icon = "\A3\ui_f\data\map\markers\nato\o_naval.paa";
-                    rarityGroup = 0.5;
-                    side = 0;
-                    class Unit0
-                    {
-                        side = 0;
-                        vehicle = "vn_o_boat_04_02";
-                        rank = "CAPTAIN";
-                        position[] = {0,0,0};
-                    };
-                };
-            }
         };
-        class VN_VC
-        {
-            class Naval
-            {
-                name = "Single Boat Groups";
-                class vn_o_group_boat_vcmf_07
-                {
-                    name = $STR_VN_O_GROUP_BOAT_VCMF_01;
-                    faction = "O_VC";
-                    icon = "\A3\ui_f\data\map\markers\nato\o_naval.paa";
-                    rarityGroup = 0.5;
-                    side = 0;
-                    class Unit0
-                    {
-                        side = 0;
-                        vehicle = "vn_o_boat_01_mg_00";
-                        rank = "CAPTAIN";
-                        position[] = {0,0,0};
-                    };
-                };
-                class vn_o_group_boat_vcmf_08
-                {
-                    name = $STR_VN_O_GROUP_BOAT_VCMF_01;
-                    faction = "O_VC";
-                    icon = "\A3\ui_f\data\map\markers\nato\o_naval.paa";
-                    rarityGroup = 0.5;
-                    side = 0;
-                    class Unit0
-                    {
-                        side = 0;
-                        vehicle = "vn_o_boat_02_mg_00";
-                        rank = "CAPTAIN";
-                        position[] = {0,0,0};
-                    };
-                };
-            }
-        };*/
     };
 
     class West
@@ -2889,44 +2815,6 @@ class CfgGroups
                 };
             };
         };
-        /*
-        class VN_MACV
-        {
-            class Naval
-            {
-                name = "Single Boat Groups";
-                class vn_b_group_boat_03
-                {
-                    name = $STR_VN_B_GROUP_BOAT_01;
-                    faction = "B_MACV";
-                    icon = "\A3\ui_f\data\map\markers\nato\b_naval.paa";
-                    rarityGroup = 0.5;
-                    side = 1;
-                    class Unit0
-                    {
-                        side = 1;
-                        vehicle = "vn_b_boat_06_02";
-                        rank = "CAPTAIN";
-                        position[] = {0,0,0};
-                    };
-                };
-                class vn_b_group_boat_04
-                {
-                    name = $STR_VN_B_GROUP_BOAT_01;
-                    faction = "B_MACV";
-                    icon = "\A3\ui_f\data\map\markers\nato\b_naval.paa";
-                    rarityGroup = 0.5;
-                    side = 1;
-                    class Unit0
-                    {
-                        side = 1;
-                        vehicle = "vn_b_boat_05_02";
-                        rank = "CAPTAIN";
-                        position[] = {0,0,0};
-                    };
-                };
-            }
-        };*/
     };
 
     class Indep
