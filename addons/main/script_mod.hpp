ebf9d2e6ddac0302df42fda28a34a74545d0cdff
Darrell Martin <darrell@briarmedia.biz>
2021-10-11 13:40:33 +0100

    [ALL] Updated clusterbuild 2.064.148221 - Public Stable 2.06

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -3,12 +3,12 @@
 
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
 #define MAJOR 1
-#define MINOR 14
-#define PATCHLVL 1
+#define MINOR 15
+#define PATCHLVL 0
 // BUILD should be YYMMDDR eg 1311211
-#define BUILD 2105061
+#define BUILD 2110111
 
-#define CLUSTERBUILD "Arma 3","Arma3",204,147719,"Stable"
+#define CLUSTERBUILD "Arma 3","Arma3",206,148221,"Stable"
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
