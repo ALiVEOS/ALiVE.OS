69dccf2b624757bc8868220e152e4835b5397db6
Darrell Martin <darrell@briarmedia.biz>
2022-07-12 15:57:51 +0100

    [ALL] Updated version to 1.15.1.2207121 - Compatibility update for PF S.O.G v1.2 release

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -4,9 +4,9 @@
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
 #define MAJOR 1
 #define MINOR 15
-#define PATCHLVL 0
+#define PATCHLVL 1
 // BUILD should be YYMMDDR eg 1311211
-#define BUILD 2204081
+#define BUILD 2207121
 
 #define CLUSTERBUILD "Arma 3","Arma3",208,149102,"Stable"
 
