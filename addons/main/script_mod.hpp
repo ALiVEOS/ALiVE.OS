5af74f4b1205d9ab172784fc3ec4d4beb2923cc8
Darrell Martin <darrell@briarmedia.biz>
2023-07-25 12:01:38 +0100

    [ALL] Updated version to 1.21.0.2307251 - Clusterbuild 2.12.150779 - Public Stable (Game Update 2.12 Creator DLC Support: Spearhead 1944)

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -3,12 +3,12 @@
 
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
 #define MAJOR 1
-#define MINOR 20
+#define MINOR 21
 #define PATCHLVL 0
 // BUILD should be YYMMDDR eg 1311211
-#define BUILD 2306131
+#define BUILD 2307251
 
-#define CLUSTERBUILD "Arma 3","Arma3",212,150301,"Stable"
+#define CLUSTERBUILD "Arma 3","Arma3",212,150779,"Stable"
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
