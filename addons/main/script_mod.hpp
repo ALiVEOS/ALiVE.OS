c20a4c9723dc1dac88f45eaa07f20880db1c51ff
Darrell Martin <darrell@briarmedia.biz>
2023-07-25 17:14:16 +0100

    [ALL] Updated version to 1.21.0.2307252

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -6,7 +6,7 @@
 #define MINOR 21
 #define PATCHLVL 0
 // BUILD should be YYMMDDR eg 1311211
-#define BUILD 2307251
+#define BUILD 2307252
 
 #define CLUSTERBUILD "Arma 3","Arma3",212,150779,"Stable"
 
