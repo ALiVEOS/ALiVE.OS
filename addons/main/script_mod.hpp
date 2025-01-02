57b8fb5b1e1d7117a5ecb9da0ac30fa9ec3f834a
Darrell Martin <darrell@briarmedia.biz>
2025-01-02 11:38:53 +0000

    [ALL] Updated ALiVE version to 2.1.8.2501021 - Clusterbuild 2.18.152405 - A3 Public Stable

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -4,9 +4,9 @@
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
 #define MAJOR 2
 #define MINOR 1
-#define PATCHLVL 7
+#define PATCHLVL 8
 // BUILD should be YYMMDDR eg 1311211
-#define BUILD 2412121
+#define BUILD 2501021
 
 #define CLUSTERBUILD "Arma 3","Arma3",218,152405,"Stable"
 
